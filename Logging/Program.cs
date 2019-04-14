cbf29f76c5179700e236daedcab4a90dfdfc43a0
Kees Hiemstra <chi@xs4all.nl>
2019-04-14 20:22:24 +0200

    Add project files.

---
+++ Logging/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logging
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(args.Length);
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+  }
+}
