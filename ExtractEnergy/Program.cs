d0185747995964e51d1638eab71495a6433c8178
Kees Hiemstra <chi@xs4all.nl>
2019-05-10 16:24:24 +0200

    Extract energy

---
+++ ExtractEnergy/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CHiDateTimeWeekNumber;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractEnergy
+{
+  class Program
+  {
+    public static List<JournalRecord> Records = new List<JournalRecord>();
+    
+    static void Main(string[] args)
+    {
+      ReadJournal();
+      SplitLevels();
+      CalculateLevels();
+      WriteRecords();
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+
+    private static void SplitLevels()
+    {
+      string[] elements;
+      string[] value;
+      int level = 0;
+      foreach (var record in Records)
+      {
+        elements = record.Levels.Split(',');
+
+        value = elements[0].Split(' ');
+        int.TryParse(value[1], out level);
+        record.LevelGas = level;
+
+        value = elements[1].Trim().Split(' ');
+        int.TryParse(value[1], out level);
+        record.LevelLow = level;
+
+        value = elements[2].Trim().Split(' ');
+        int.TryParse(value[1], out level);
+        record.LevelHigh = level;
+      }
+    }
+
+    private static void CalculateLevels()
+    {
+      DateTime lastDate = DateTime.Now;
+      int lastGas = 0, lastLow = 0, lastHigh = 0;
+
+      foreach (var record in Records)
+      {
+        DateTimeWeekNumber dwn = new DateTimeWeekNumber(record.Date);
+        record.Week = dwn.ISOWeekNoExtended;
+
+        if (lastGas != 0)
+        {
+          record.Time = record.Date - lastDate;
+          record.Gas = record.LevelGas - lastGas;
+          record.Low = record.LevelLow - lastLow;
+          record.High = record.LevelHigh - lastHigh;
+          if (record.Gas < 0)
+          {
+            record.Gas = record.LevelGas;
+            record.Low = record.LevelLow;
+            record.High = record.LevelHigh;
+          }
+        }
+        record.Current = record.Low + record.High;
+        lastDate = record.Date;
+        lastGas = record.LevelGas;
+        lastLow = record.LevelLow;
+        lastHigh = record.LevelHigh;
+      }
+    }
+
+    private static void WriteRecords()
+    {
+      using (StreamWriter stream = new StreamWriter(@"C:\Users\Kees\OneDrive\Data\Energie.csv"))
+      {
+        stream.WriteLine("Date,Week,Gas,Laag,Hoog,Gas,Stroom");
+        foreach (var record in Records)
+        {
+          stream.WriteLine($"{record.Date},{record.Week}," +
+            $"{record.LevelGas},{record.LevelLow},{record.LevelHigh}," +
+            $"{record.Gas},{record.Current}");
+        }
+      }
+    }
+
+    private static void ReadJournal()
+    {
+      using (StreamReader stream = new StreamReader(@"C:\Users\Kees\OneDrive\Data\Energy.csv"))
+      {
+        int count = 0;
+        while (!stream.EndOfStream)
+        {
+          count++;
+          string Line = stream.ReadLine();
+          ProcessLine(Line, count);
+        }
+      }
+    }
+
+    private static void ProcessLine(string line, int count)
+    {
+      string[] Elements = line.Replace("\"", "").Split(';');
+
+      if (count > 1)
+      {
+        Records.Add(new JournalRecord
+        {
+          Date = DateTime.Parse(Elements[0]),
+          Levels = Elements[1]
+        });
+      }
+    }
+
+
+  }
+
+  public class JournalRecord
+  {
+    public DateTime Date { get; set; }
+    public string Week { get; set; }
+    public string Levels { get; set; }
+    public int LevelGas { get; set; }
+    public int LevelLow { get; set; }
+    public int LevelHigh { get; set; }
+    public TimeSpan Time { get; set; }
+    public int Gas { get; set; }
+    public int Low { get; set; }
+    public int High { get; set; }
+    public int Current { get; set; }
+    public int WeekGas { get; set; }
+    public int WeekLow { get; set; }
+    public int WeekHigh { get; set; }
+  }
+}
---
+++ Pivot1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pivot1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Record> records = new List<Record>();
+      records.Add(new Record { Month = "19-01", TallyName = "sub1", Account = "02", Amount = 13 });
+      records.Add(new Record { Month = "19-01", TallyName = "sub3", Account = "05", Amount = 5 });
+      records.Add(new Record { Month = "19-01", TallyName = "sub2", Account = "03", Amount = 11 });
+      records.Add(new Record { Month = "19-01", TallyName = "sub3", Account = "04", Amount = 7 });
+      records.Add(new Record { Month = "19-02", TallyName = "sub4", Account = "06", Amount = 19 });
+      records.Add(new Record { Month = "19-02", TallyName = "sub3", Account = "02", Amount = 29 });
+      records.Add(new Record { Month = "19-02", TallyName = "add1", Account = "01", Amount = 100 });
+      records.Add(new Record { Month = "19-02", TallyName = "sub3", Account = "03", Amount = 23 });
+      records.Add(new Record { Month = "19-01", TallyName = "add1", Account = "01", Amount = 200 });
+      records.Add(new Record { Month = "19-02", TallyName = "sub4", Account = "07", Amount = 17 });
+
+      var list2 = (from l in records
+                   orderby l.Month, l.TallyName
+                   group l by new { l.Month, l.TallyName}
+                   into x
+                     select new
+                     {
+                       x.Key.Month,
+                       x.Key.TallyName,
+                       //x.Key.Account,
+                       s = x.Sum(y => y.Amount)
+                     }
+                   ).ToList();
+
+      foreach (var rec in list2)
+      {
+        foreach (var item in rec.GetType().GetProperties())
+        {
+          Console.Write($"{item.GetValue(rec).ToString()}\t");
+        }
+        Console.WriteLine();
+      }
+
+
+      //var list1 = (from l in records
+      //            select l
+      //            ).ToList();
+
+      //foreach (var rec in list1)
+      //{
+      //  foreach (var item in rec.GetType().GetProperties())
+      //  {
+      //    Console.Write($"{item.GetValue(rec).ToString()}\t");
+      //  }
+      //  Console.WriteLine();
+      //}
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+
+
+  }
+
+  public class Record
+  {
+    public string Month { get; set; }
+    public string TallyName { get; set; }
+    public string Account { get; set; }
+    public int Amount { get; set; }
+  }
+}
