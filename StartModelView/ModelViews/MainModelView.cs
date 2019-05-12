494a40d8a143a79d57fa2e76a0757af9917d6271
Kees Hiemstra <chi@xs4all.nl>
2019-05-12 12:15:56 +0200

    StartModelView

---
+++ StartModelView/ModelViews/MainModelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartModelView.ModelViews
+{
+  public class MainModelView
+  {
+
+    public MainModelView()
+    {
+      MainWindow Main = new MainWindow();
+      Main.Show();
+    }
+  }
+}
